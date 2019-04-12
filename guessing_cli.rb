c520a490ad7471a46868d6755b7710f2142b4953
Sarah Anderson <saranderson13@gmail.com>
2019-04-12 04:35:04 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,10 +3,15 @@ def exit_game
 end
 
 def run_guessing_game
-  correct_answer = Random.new.rand(1..6)
+  correct_answer = rand(1..6)
   
   puts "Guess a number between 1 and 6."
   selection = gets.chomp
-  
+  case selection
+    when selection == correct_answer
+      puts "You guessed the correct number!"
+    when selection != correct_answer
+      puts "The computer guessed #{correct_answer}"
+  end
   exit_game if selection == "exit"
 end
